d7d035cdb9d0c0794fe923c96cb1eb9067aca657
Rembrant Pasardan <160525899+BrantySis@users.noreply.github.com>
2024-11-30 01:22:55 +0800

    toast and spa installation

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import "./bootstrap";
 import "preline";
 import toastr from 'toastr';
 import 'toastr/build/toastr.min.css';
+import Notifications from 'vue-notification';
+import velocity from 'velocity-animate';
 // import Alpine from "alpinejs";
 
 // window.Alpine = Alpine;
@@ -19,3 +21,10 @@ if (
 } else {
     document.documentElement.classList.remove("dark");
 }
+Vue.use(Notifications, { velocity });
+Vue.component('toaster-group', require('laralabs-vue-toaster/src/ToasterGroupComponent.vue'));
+Vue.component('toaster-logic', require('laralabs-vue-toaster/src/ToasterLogicComponent.vue'));
+
+let app = new Vue({
+  el: '#app',
+});
